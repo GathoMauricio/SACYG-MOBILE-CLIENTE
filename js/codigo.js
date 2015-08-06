ca07af18e5bfb48682bd9cb013c2ddfb68f88c35
GathoMauricio <mauricio2769@gmail.com>
2015-08-05 23:26:46 -0500

    mapa

--- js/codigo.js
+++ js/codigo.js
@@ -273,7 +273,7 @@ function loadPedido()
 			$("#contenedor_pedido").html("<center><h3>Su solicitud se encuentra pendiente</h3></center>");
 			break;
 			case "2":
-			$("#contenedor_pedido").html("<center><p><h3>Su solicitud se encuentra en proceso</h3></p><p><h3 onclick='verRuta();'><span class='icon-map2'></span> Ver ruta del repartidor<img src='img/iconos/map.png' width='80%' onclick='verRuta();'></h3></p></center>");
+			$("#contenedor_pedido").html("<center><p><h3>Su solicitud se encuentra en proceso</h3></p><p><h3 onclick='verRuta();'><span class='icon-map2'></span> Ver ruta del repartidor<img src='img/iconos/map.png' width='80%' onclick='verRuta();'></h3></p><a href='geo:38.897096,-77.036545'>open map</a></center>");
 			break;
 		}
 	});
